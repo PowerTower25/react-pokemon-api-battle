1408d9598c5248606a1f81d27d9c54d612748c71
Jacob Grady <jacobgrady@MacBook-Pro.lan>
2026-01-23 18:37:04 -0500

    feat(card): added code for running mulitple cards

--- src/FormField.tsx
+++ src/FormField.tsx
@@ -5,7 +5,7 @@ const apiURL = "https://api.pokemontcg.io/v2/cards?pageSize=1&page=100"
 
   
 function FormField() {
-  const [card, setCard] = useState(null);
+  const [cards, setCard] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -14,10 +14,10 @@ const fetchRandomCard = async () => {
     try {
       // Fetch a random page, assuming ~15k+ cards, 1 per page
       const randomPage = Math.floor(Math.random() * 1000) + 1;
-      const response = await fetch(`https://api.pokemontcg.io/v2/cards?page=${randomPage}&pageSize=1`);
+      const response = await fetch(`https://api.pokemontcg.io/v2/cards?page=${randomPage}&pageSize=2`);
       const data = await response.json();
-      console.log(data.data[0])
-      setCard(data.data[0]); // Set the first card from the result
+      setCard(data.data); // Set the first card from the result
+      console.log(data.data)
     } catch (error) {
       console.error("Error fetching card:", error);
     }
@@ -37,10 +37,10 @@ const fetchRandomCard = async () => {
     return (
         <>
         <button onClick={fetchRandomCard}>{loading ? 'Loading' : 'Get a card'}</button>
-        {card && (
-            <Card name={card.name} hp={card.hp} attacks={card.attacks}/>
-        )}
-             
+
+          {cards && cards.map((card) => {
+            return <Card key={card.id} name={card.name} hp={card.hp} attacks={card.attacks}/>
+          })}
         </>
     )
 }
